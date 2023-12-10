6c196003df364dedf0456ecf1aebcf23d7934a62
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-12-10 23:47:06 +0600

    poppins google fonts added

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => {
     return (
-        <div className="max-w-7xl mx-auto">
+        <div className="max-w-7xl mx-auto bg-gray-800 text-gray-100 ">
             <Outlet></Outlet>
         </div>
     );
--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
+import Home from "./components/Home/Home";
 
 const router = createBrowserRouter([
     {
       path: "/",
       element: <MainLayout></MainLayout>,
+      children: [
+        {
+            path: "/",
+            element: <Home></Home>
+        }
+      ]
     },
   ]);
 
