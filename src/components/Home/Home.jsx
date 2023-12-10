4a4f9083c71bd5fe6a0bf24f86b49b1e73e86a15
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-12-11 00:35:19 +0600

    Navbar component added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Navbar from "../Navbar/Navbar";
 
 
 const Home = () => {
     return (
         <div>
-            <h2>This is home</h2>
+            <Navbar></Navbar>
+            
         </div>
     );
 };
